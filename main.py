47d8cb3a3da848dec12446a48c4c798e4fe0be16
Sergio <ignite21@protonmail.com>
2020-04-03 19:04:21 +0200

    continues working...

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from PIL import Image, ImageTk
 
 class MainClass(tk.Tk):
     def __init__(self, Root, *args, **kwargs):
-        self.Root = Root
-        self.Root.title('Getit')
-        self.Root.geometry('500x500')
+        self._root = Root
+        self._root.title('Getit')
+        self._root.geometry('500x500')
 
 def Main():
-    Root = tk.Tk()
-    Getit = MainClass(Root)
-    Root.mainloop()
+    root = tk.Tk()
+    getit = MainClass(root)
+    root.mainloop()
 
 if __name__ == '__main__':
     Main()
