33fe6f442afca161af310a4aeabf1cc245203dab
Sergio <ignite21@protonmail.com>
2020-04-12 19:28:18 +0200

    continues...

--- connection_db.py
+++ connection_db.py
@@ -30,17 +30,27 @@ class ConnectionClass(object):
         try:
             cursor_db.execute(
                 ''' CREATE TABLE backups (
-                        id INT AUTO_INCREMENT,
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        url VARCHAR(250) NOT NULL,
                         type VARCHAR(30) NOT NULL,
                         rename VARCHAR(100) NOT NULL,
                         path VARCHAR(100) NOT NULL,
                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        active TINYINT(1) NOT NULL DEFAULT '1',
-                        PRIMARY KEY (id)  
+                        active TINYINT(1) NOT NULL DEFAULT '1'
                 ) '''
             )
 
         except sqlite3.OperationalError:
-            pass
+            _DATES = (
+                _URL.get(),
+                _TYPES.get(),
+                _RENAME.get(),
+                _PATH_DIR
+            )
+            
+            if _URL.get() != '' and _TYPES != 'Types':
+                cursor_db.execute('INSERT INTO backups (url, type, rename, path) VALUES (?, ?, ?, ?)', _DATES)
                 
-        connect_db.close() # Connection closed
+        finally:
+            connect_db.commit()
+            connect_db.close() # Connection closed
